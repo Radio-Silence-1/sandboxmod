2071276ed2a9a9f12dc31cfb35561d35d2ba17f0
Radio-Silence-1 <98431859+Radio-Silence-1@users.noreply.github.com>
2023-10-19 09:53:26 -0500

    Add files via upload

---
+++ idonthinkitwork.js
@@ -0,0 +1,22 @@
+
+elements.thisisatest = {
+    color: "#006e05",
+	
+	tempHigh: 1000,
+    stateHigh: "party_popper",
+    tempLow: -130,
+    stateLow: "grenade",
+	
+	 behavior: [
+        "XX|XX|XX",
+        "XX|CR:dirt|XX",
+        "M2|M1 AND SW|M2",
+    ],
+    burnTime: 150,
+    burnInto: "explosion",
+    burning: true,
+    category: "land",
+    state: "solid",
+	 temp:68,
+	 cooldown:1,
+}; 
